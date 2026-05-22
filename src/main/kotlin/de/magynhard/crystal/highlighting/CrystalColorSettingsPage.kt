7c0d75254b6f738042495fac4cfda8ff21b59e52
magynhard <github@mail.magynhard.de>
2026-05-22 14:53:43 +0200

    feat: add Color Settings Page for Crystal syntax colors
    Adds Settings > Editor > Color Scheme > Crystal with configurable
    colors for all token types (keywords, strings, numbers, symbols,
    variables, operators, etc.) with a live preview demo.

---
+++ src/main/kotlin/de/magynhard/crystal/highlighting/CrystalColorSettingsPage.kt
@@ -0,0 +1,94 @@
+package de.magynhard.crystal.highlighting
+
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.fileTypes.SyntaxHighlighter
+import com.intellij.openapi.options.colors.AttributesDescriptor
+import com.intellij.openapi.options.colors.ColorDescriptor
+import com.intellij.openapi.options.colors.ColorSettingsPage
+import de.magynhard.crystal.CrystalIcons
+import javax.swing.Icon
+
+class CrystalColorSettingsPage : ColorSettingsPage {
+
+    companion object {
+        private val DESCRIPTORS = arrayOf(
+            AttributesDescriptor("Keyword", CrystalSyntaxHighlighter.KEYWORD),
+            AttributesDescriptor("Number", CrystalSyntaxHighlighter.NUMBER),
+            AttributesDescriptor("String", CrystalSyntaxHighlighter.STRING),
+            AttributesDescriptor("Character", CrystalSyntaxHighlighter.CHAR),
+            AttributesDescriptor("Comment", CrystalSyntaxHighlighter.COMMENT),
+            AttributesDescriptor("Identifier", CrystalSyntaxHighlighter.IDENTIFIER),
+            AttributesDescriptor("Constant", CrystalSyntaxHighlighter.CONSTANT),
+            AttributesDescriptor("Instance variable", CrystalSyntaxHighlighter.INSTANCE_VAR),
+            AttributesDescriptor("Class variable", CrystalSyntaxHighlighter.CLASS_VAR),
+            AttributesDescriptor("Global variable", CrystalSyntaxHighlighter.GLOBAL_VAR),
+            AttributesDescriptor("Symbol", CrystalSyntaxHighlighter.SYMBOL),
+            AttributesDescriptor("Operator", CrystalSyntaxHighlighter.OPERATOR),
+            AttributesDescriptor("Regular expression", CrystalSyntaxHighlighter.REGEX),
+            AttributesDescriptor("String interpolation", CrystalSyntaxHighlighter.INTERPOLATION),
+            AttributesDescriptor("Parentheses", CrystalSyntaxHighlighter.PARENTHESES),
+            AttributesDescriptor("Brackets", CrystalSyntaxHighlighter.BRACKETS),
+            AttributesDescriptor("Braces", CrystalSyntaxHighlighter.BRACES),
+            AttributesDescriptor("Comma", CrystalSyntaxHighlighter.COMMA),
+            AttributesDescriptor("Semicolon", CrystalSyntaxHighlighter.SEMICOLON),
+            AttributesDescriptor("Dot", CrystalSyntaxHighlighter.DOT),
+            AttributesDescriptor("Bad character", CrystalSyntaxHighlighter.BAD_CHARACTER),
+        )
+    }
+
+    override fun getIcon(): Icon = CrystalIcons.FILE
+
+    override fun getHighlighter(): SyntaxHighlighter = CrystalSyntaxHighlighter()
+
+    override fun getDemoText(): String = """
+        # Crystal example
+        class Greeter
+          @name : String
+          @@count = 0
+
+          def initialize(@name : String)
+            @@count += 1
+          end
+
+          def greet(greeting = "Hello")
+            puts "#{greeting}, #{@name}!"
+          end
+        end
+
+        module Printable
+          abstract def to_s : String
+        end
+
+        enum Color
+          Red; Green; Blue
+        end
+
+        # Literals
+        number = 42_i64
+        float = 3.14
+        char = 'a'
+        symbol = :my_symbol
+        regex = /[a-z]+/i
+        arr = %w(foo bar baz)
+        heredoc = <<-END
+          content
+          END
+
+        # Control flow
+        if number > 0
+          result = number ** 2
+        end
+
+        [1, 2, 3].each do |item|
+          puts item
+        end
+    """.trimIndent()
+
+    override fun getAdditionalHighlightingTagToDescriptorMap(): Map<String, TextAttributesKey>? = null
+
+    override fun getAttributeDescriptors(): Array<AttributesDescriptor> = DESCRIPTORS
+
+    override fun getColorDescriptors(): Array<ColorDescriptor> = ColorDescriptor.EMPTY_ARRAY
+
+    override fun getDisplayName(): String = "Crystal"
+}
