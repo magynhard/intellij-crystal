364d80f5edbb6d009c219aaf43829b60cb825fa9
magynhard <github@mail.magynhard.de>
2026-07-05 16:37:47 +0200

    refactor: replace deprecated DefaultLiveTemplatesProvider with declarative defaultLiveTemplates extension point

--- src/main/kotlin/de/magynhard/crystal/CrystalLiveTemplateProvider.kt
+++
@@ -1,8 +0,0 @@
-package de.magynhard.crystal
-
-import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider
-
-class CrystalLiveTemplateProvider : DefaultLiveTemplatesProvider {
-    override fun getDefaultLiveTemplateFiles(): Array<String> = arrayOf("/liveTemplates/Crystal")
-    override fun getHiddenLiveTemplateFiles(): Array<String>? = null
-}
